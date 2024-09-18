0609909ba987eb82e6accdec62d694dd9db298f4
bishopfunc <kuaiwanga@gamil.com>
2024-09-18 14:38:56 +0900

    zenn-embed-elementsはSSRで使えない

--- src/app/editor.tsx
+++ src/app/editor.tsx
@@ -4,14 +4,18 @@ import 'easymde/dist/easymde.min.css';
 import { SetStateAction, useState } from 'react';
 import markdownToHtml from 'zenn-markdown-html';
 import 'zenn-content-css';
-import MdBodyRender from '../components/MdBodyRender';
-
+import { useEffect } from 'react';
 
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
   ssr: false,
 });
 
 const MarkdownEditorWithPreview = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      import('zenn-embed-elements');
+    }
+  }, []);  
   const [text, setText] = useState('');
 
   const handleChange = (value: SetStateAction<string>) => {
@@ -24,7 +28,7 @@ const MarkdownEditorWithPreview = () => {
         <SimpleMDE value={text} onChange={handleChange} spellCheck={false} />
       </div>
       <div className="w-1/2 p-4 bg-gray-100">
-        <MdBodyRender html={htmlRes} /> 
+        <div className="znc" dangerouslySetInnerHTML={{ __html: htmlRes }} />
       </div>
     </div>
   );
--- src/components/MdBodyRender.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import 'zenn-embed-elements';
-
-export default function MdBodyRender(props: {html: string}) {
-    return (
-        <div className="znc" dangerouslySetInnerHTML={{ __html: props.html }} />
-    )
-}
